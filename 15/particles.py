1f998972cd419558a35a1d69895f2801a0a55cae
Andrin Rehmann <andrinrehmann@gmail.com>
2021-03-08 16:25:42 +0100

    finsiedh prioq

---
+++ 15/particles.py
@@ -0,0 +1,135 @@
+import random as rd
+import math as math
+import time
+import matplotlib.pyplot as plt
+from prioq import prioq
+
+# Binary tree
+class Cell:
+
+    def __init__(self, parent, dimension, left, right, particles, boundA, boundB):
+        self.left = left
+        self.right = right
+        self.boundA = boundA
+        self.boundB = boundB
+        # posX, posY, mass
+        self.particles = particles
+        self.parent = parent
+        self.dimension = dimension
+        self.isLeaf = False
+        self.splitPosition = 0
+
+        if (self.right - self.left > 64):
+            self.partition()
+        else:
+            self.isLeaf = True
+            # Determine radius, distance from center to particle furthest away
+            self.radius = 0
+            for particle in self.particles[self.left : self.right]:
+                self.radius = max(self.radius, self.dist(self.center(), particle))
+
+    # O(n*log(n)), when executed on root
+    def partition(self):
+        # random initial guess
+        guess = (self.boundA[self.dimension] + self.boundB[self.dimension])/2
+        count = self.right - self.left
+        halfCount = round(count / 2)
+
+        # binary search over float, 64bit
+        # Starting at 4 because range is between 0 and 1
+        # Initial guess is at 0.5, thus next guess 
+        for i in range(2, 64, 1):
+            nLeft = 0
+            for j in range(self.left, self.right):
+                # branchless counting
+                nLeft += (self.particles[j][self.dimension] < guess)
+
+            # guess improvement
+            guess += ((nLeft < halfCount) - (nLeft > halfCount)) * 1/(2<<i)
+
+            # assuming power of two total particle count, where power >= 3
+            # assuming unique particle positions
+            # probablity for not unqiue random float positions is ~0
+            if(abs(nLeft - halfCount) == 0):
+                break
+        
+        # single iteration of quicksort, O(n)
+        i = 0
+        j = count - 1
+        while(i < halfCount and j >= halfCount):
+            if (self.particles[self.left + i][self.dimension] > guess):
+                tmp = self.particles[self.left + j]
+                self.particles[self.left + j] = self.particles[self.left + i]
+                self.particles[self.left + i] = tmp
+                j -= 1
+            else:
+                i += 1
+
+        self.splitPosition = guess
+
+        newBoundA = self.boundA.copy()
+        newBoundB = self.boundB.copy()
+        newBoundA[self.dimension] = self.splitPosition
+        newBoundB[self.dimension] = self.splitPosition
+        self.childA = Cell(self, 1-self.dimension, self.left, self.left + halfCount, self.particles, self.boundA, newBoundB)
+        self.childB = Cell(self, 1-self.dimension, self.left + halfCount, self.right, self.particles, newBoundA, self.boundB)
+
+        # Compute radius of non leaf cell
+        center = self.center()
+        centerA = self.childA.center()
+        centerB = self.childB.center()
+        self.radius = max(self.dist(center, centerA) + self.childA.radius, self.dist(center, centerB) + self.childB.radius)
+
+    # Get center of cell, in this case center of square, not average of particles
+    def center(self):
+        return [(self.boundA[0] + self.boundB[0]) / 2, (self.boundA[1] + self.boundB[1]) / 2]
+
+    # Distance function
+    # In this case using a circle, not a box
+    def dist(self, a, b):
+        x = abs(a[0] - b[0])
+        y = abs(a[1] - b[1])
+        # Periodic boundaries
+        x = min(x, 1-x)
+        y = min(y, 1-y)
+        return math.sqrt(x * x + y * y)
+
+    def kNearest(self, position, queue):
+        if self.isLeaf:
+            for particle in self.particles:
+                d = self.dist(particle, position) + self.radius
+                if d < queue.getMax():
+                    print(".")
+                    queue.insert(d, particle)
+
+        else:
+            # Max distances from position outermost particle from child cells
+            distA = self.dist(position, self.childA.center()) + self.radius
+            distB = self.dist(position, self.childB.center()) + self.radius
+            if distA < distB:
+                if distA < queue.getMax():
+                    self.childA.kNearest(position, queue)
+                if distB < queue.getMax():
+                    self.childB.kNearest(position, queue)
+            else:
+                if distB < queue.getMax():
+                    self.childB.kNearest(position, queue)
+                if distA < queue.getMax():
+                    self.childA.kNearest(position, queue)
+
+def buildTree(num):
+    particles = []
+    for i in range(num):
+        particles.append([rd.random(), rd.random(), rd.random()])
+
+    return Cell(False, 0, 0, num, particles, [0,0], [1,1])
+
+num = 2 << 14
+root = buildTree(num)
+
+queue = prioq(32)
+
+root.kNearest([0.5, 0.5], queue)
+
+print(queue.data)
+
